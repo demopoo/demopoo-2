b9ecdfc65298b9e28660a93e468e5fca28222008
puyu <puyu@qq.com>
2018-04-09 17:18:29 +0800

    master modify

--- Empty.java
+++ Empty.java
@@ -2,6 +2,10 @@ public class Hello{
 	
 	public static void main(String[] args){
 		System.out.println("every body having test gift");
+		System.out.println("从master上面修改");
+		System.out.println("从master上面修改");
+		System.out.println("从master上面修改");
+
 	}
 }
 
