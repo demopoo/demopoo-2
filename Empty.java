ea96a279942069f71d43239f708c0088fa988fa7
puyu <puyu@qq.com>
2018-04-09 17:14:45 +0800

    branch modify

--- Empty.java
+++ Empty.java
@@ -2,6 +2,9 @@ public class Hello{
 	
 	public static void main(String[] args){
 		System.out.println("every body having test gift");
+		System.out.println("分支上面修改");
+		System.out.println("分支上面修改");
+		System.out.println("分支上面修改");
 	}
 }
 
