962f22dbe98a9e74b05a9e4f44f1a49d71c60614
puyu <puyu@qq.com>
2018-04-10 15:33:23 +0800

    change

--- em.java
+++ em.java
@@ -2,6 +2,12 @@ public class Hello{
 	
 	public static void main(String[] args){
 		System.out.println("emergency file");
+
+		System.out.println("add patch");
+		System.out.println("add patch");
+		System.out.println("add patch");
+		System.out.println("add patch");
+		System.out.println("add patch");
 	}
 }
 
